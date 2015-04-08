6db9bbe13519a89282a43e633e3a61f7c55b975f
linlinqilin <631149233@qq.com>
2015-04-08 15:11:10 +0800

    qilin

--- frontend/views/index/index.php
+++ frontend/views/index/index.php
@@ -1,41 +1,42 @@
 
 <!--图片轮播内容区-->
-<!-- Slider -->
+ <!-- Slider -->
 <div id="Adv">
-    <!--图片列表-->
-    <div class="ImgList" style="background:url('/images/maxbg1.jpg') no-repeat center top"></div>
-    <div class="ImgList" style="background:url('/images/maxbg2.jpg') no-repeat center top"></div>
-    <div class="ImgList" style="background:url('/images/maxbg3.jpg') no-repeat center top"></div>
-    <div class="ImgList" style="background:url('/images/maxbg4.jpg') no-repeat center top"></div>
-
-    <!--小图-->
-    <div class="xiaotu">
-        <ul>
-            <li class="hover"><img src="/images/minbg1.jpg" /></li>
-            <li><img src="/images/minbg2.jpg"  /></li>
-            <li><img src="/images/minbg3.jpg" /></li>
-            <li><img src="/images/minbg4.jpg" /></li>
-        </ul>
-    </div>
-
-
-    <!--轮播的按扭-->
-    <div class="ydbutton">
-        <ul class="button">
-            <li class="hover"></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </div>
-    <!--左右按钮-->
-    <div class="leftbutton">
-        <div class="btimg"><img src="/images/left.png" /></div>
-    </div>
-
-    <div class="rightbutton">
-        <div class="btimg"><img src="/images/right.png" /></div>
-    </div>
+	<!--图片列表-->
+	<div class="ImgList" style="background:url('images/maxbanner2.jpg') no-repeat center top"></div>
+	<div class="ImgList" style="background:url('images/maxbanner1.jpg') no-repeat center top"></div>
+	<div class="ImgList" style="background:url('images/maxbanner3.jpg') no-repeat center top"></div>
+	<div class="ImgList" style="background:url('images/maxbanner4.jpg') no-repeat center top"></div>
+	
+	<!--小图-->
+	<div class="xiaotu">
+		<ul>
+			<li class="hover"><img src="images/minbanner2.jpg" width="296"  height="166" /><p><img src="images/chun.png" /></p></li>
+			<li><img src="images/minbanner1.jpg"  width="296"  height="166"  /><p><img src="images/xiay.png" /></p></li>
+			<li><img src="images/minbanner3.jpg"   width="296"  height="166"  /><p><img src="images/qiu.png" /></p></li>
+			<li><img src="images/minbanner4.jpg"  width="296"  height="166" /><p><img src="images/dong.png" /></p></li>
+		</ul>
+	</div>
+	
+	
+	<!--轮播的按扭-->
+	<div class="ydbutton">
+		<ul class="button">
+			<li class="hover"></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	
+	<!--左右按钮-->
+	<div class="leftbutton">
+		<div class="btimg"></div>
+	</div>
+	
+	<div class="rightbutton">
+		<div class="btimg"></div>	
+	</div>
 </div>
 
 
